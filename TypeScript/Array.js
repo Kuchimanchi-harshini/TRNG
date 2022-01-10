0aebc27b7f3a88a1f6764b99d2dcb7a36cb5b2f2
Kuchimanchi-harshini <94523999+Kuchimanchi-harshini@users.noreply.github.com>
2022-01-10 18:19:39 +0530

    Add files via upload

---
+++ TypeScript/Array.js
@@ -0,0 +1,8 @@
+var Name = ['Harshini', 'Shireesha', 'Sowmya'];
+//for(var index in Name)
+//{
+// console.log(Name[index]);  
+//}
+for (var i = 0; i < Name.length; i++) {
+    console.log(Name[i]);
+}
