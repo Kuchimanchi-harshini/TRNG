aaf561a0edb201ec0d9482d2481ac26946ea2288
Kuchimanchi-harshini <94523999+Kuchimanchi-harshini@users.noreply.github.com>
2022-01-10 18:20:50 +0530

    Delete TypeScript directory

--- TypeScript/Address.ts
+++
@@ -1,8 +0,0 @@
-import {Student} from "./Student";
-
-
-class Address extends Student{
-
-}
-var a1 = new Address(102,"NewStudent","ECE")
-a1.display();
--- TypeScript/Array.js
+++
@@ -1,8 +0,0 @@
-var Name = ['Harshini', 'Shireesha', 'Sowmya'];
-//for(var index in Name)
-//{
-// console.log(Name[index]);  
-//}
-for (var i = 0; i < Name.length; i++) {
-    console.log(Name[i]);
-}
